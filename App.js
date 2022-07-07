9174958eeebcf5c5f1f100978844056715d94bb3
bristinborah <85515047+bristinborah@users.noreply.github.com>
2022-07-07 07:26:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Click the Button to withdraw the token.
+        </p>
+        <input placeholder="Enter amount" type='number'></input>
+        <div><button className='button'>withdraw</button></div>
+        
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
